13834f4bcdcd3e1bbc4e05db5b10db8d72934600
JHXSMatthew <68638023@qq.com>
2018-02-18 00:30:14 +1100

    init commit

---
+++ src/main/java/com/mcndsj/GameEvent/Core.java
@@ -0,0 +1,14 @@
+package com.mcndsj.GameEvent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Matthew on 2016/5/21.
+ */
+public class Core extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+}
